62e15938b6eb94a6aedf90ee01361035fb4d877e
Santiago Sanchez <sasanchezra@unal.edu.co>
2022-07-10 08:47:48 -0500

    inicio

---
+++ Cuerpo_negro.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 30 14:32:29 2022
+
+@author: santiago
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+h = 6.626e-34
+c = 3.0e+8
+k = 1.38e-23
+
+def planck(wav, T):
+    a = 2.0*h*c**2
+    b = h*c/(wav*k*T)
+    intensity = a/ ( (wav**5) * (np.exp(b) - 1.0) )
+    return intensity
+
+# generate x-axis in increments from 1nm to 3 micrometer in 1 nm increments
+# starting at 1 nm to avoid wav = 0, which would result in division by zero.
+wavelengths = np.arange(1e-9, 3e-6, 1e-9) 
+
+
+intensity5000 = planck(wavelengths, 5000.)
+
+
+
+plt.plot(wavelengths*1e9, intensity5000, 'k-') # 5000K green line
+
+# show the plot
+plt.show()
